569a8b9b7c8174a7b7250b1866cc1ee5bc05199e
Spyro1001 <raymondhedgecock@gmail.com>
2022-09-08 19:32:54 -0700

    adding inheritance

---
+++ src/main/java/org/example/inheritance/animal/Bird.java
@@ -0,0 +1,17 @@
+package org.example.inheritance.animal;
+
+public class Bird extends Animal {
+
+    public void setAge(int age) {
+        this.age = age + 10;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Bird {");
+        sb.append("age=")
+          .append(age);
+        sb.append('}');
+        return sb.toString();
+    }
+}
---
+++ src/main/java/org/example/inheritance/animal/Zookeeper.java
@@ -0,0 +1,22 @@
+package org.example.inheritance.animal;
+
+public class Zookeeper {
+
+    public static void main(String[] args) {
+
+        Animal bird = new Bird();
+        Animal fish = new Fish();
+        Animal reptile = new Reptile();
+
+        bird.setAge(12);
+        fish.setAge(2);
+        reptile.setAge(10);
+
+        ((Fish)fish).swim();
+
+        System.out.println(bird);
+        System.out.println(fish);
+        System.out.println(reptile);
+
+    }
+}
